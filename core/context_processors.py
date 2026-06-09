9a6138e23fa681475e5241ce9e10178fe5f293cb
Carlos Lopez <clopez.ti@vcchile.cl>
2026-06-08 21:44:59 -0400

    fix: python312 en nixpacks, context_processors SENTINEL vars

--- core/context_processors.py
+++ core/context_processors.py
@@ -2,6 +2,6 @@ from django.conf import settings
 
 def perseus_context(request):
     return {
-        "PERSEUS_COMPANY_NAME": getattr(settings, "PERSEUS_COMPANY_NAME", "Perseus Technology"),
-        "PERSEUS_SUPPORT_EMAIL": getattr(settings, "PERSEUS_SUPPORT_EMAIL", "soporte@perseustechnology.dev"),
+        "SENTINEL_COMPANY_NAME": getattr(settings, "SENTINEL_COMPANY_NAME", "Sentinel XO"),
+        "SENTINEL_SUPPORT_EMAIL": getattr(settings, "SENTINEL_SUPPORT_EMAIL", "soporte@perseustechnology.dev"),
     }
